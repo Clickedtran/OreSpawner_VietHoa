216da7bfab399ea4ca25d7e695d1a0bf1a4491a4
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-01-01 19:44:20 +0700

    Create CreateOreSpawnerEvent.php

---
+++ src/vennv/orespawner/events/CreateOreSpawnerEvent.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace vennv\orespawner\events;
+
+use pocketmine\event\Event;
+
+use pocketmine\event\Cancellable;
+
+use pocketmine\player\Player;
+
+use pocketmine\world\Position;
+
+class CreateOreSpawnerEvent extends Event implements Cancellable{
+
+    private bool $isCancelled = false;
+
+    public function __construct(
+
+        private ?Player $player,
+
+        private Position $position
+
+    ){}
+
+    public function getPlayer() : ?Player{
+
+        return $this->player;
+
+    }
+
+    public function getPosition() : Position{
+
+        return $this->position;
+
+    }
+
+    public function isCancelled() : bool{
+
+        return $this->isCancelled;
+
+    }
+
+    public function cancel() : void{
+
+        $this->isCancelled = true;
+
+    }
+
+    public function uncancel() : void{
+
+        $this->isCancelled = false;
+
+    }
+
+}
+
